2c6ba4860ed25fbace5e987094b7d6575a4c763e
Luis Eckert <eckert.luis@gmx.de>
2022-01-04 23:45:48 +0100

    Update login.inc.php

--- src/inc/login.inc.php
+++ src/inc/login.inc.php
@@ -1,19 +1,26 @@
 <?php
     if(isset($_POST)){
-        $name = addslashes($_POST["uid"]);
+        $uid = addslashes($_POST["uid"]);
         $pwd =  addslashes($_POST["pwd"]);
         
         require_once("db/dhb.inc.php");
         require_once("db/db.user.uidExists.inc.php");
+        require_once("db/db.user.verifyPassword.inc.php");
         require_once("pwd.inc.php");
         require_once("login-functions.inc.php");
 
-        if(emptyInputLogin($name, $pwd) !== false){
+        if(emptyInputLogin($uid, $pwd) !== false){
             header("location: ../login.php?err=emptyInput");
             exit();
         }
 
-
+        if(!verifyLogin($DB_Spellbook, $uid, $pwd)){
+            header("location: ../login.php?err=incorrectLogin");
+            exit();
+        }
+        
+        $_SESSION['current_user_id'] = '';
+        header("location: ../choosechar.php?msg=successfullLogin");
 
     }else{
         header("location: ../login.php");
