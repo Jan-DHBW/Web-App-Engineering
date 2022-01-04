4f8a5edb42f78fd14c5814eafcfb2f2201a44e39
Luis Eckert <eckert.luis@gmx.de>
2022-01-04 22:01:40 +0100

    Delete choosechar.inc.php

--- src/inc/choosechar.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-    function getCharactersByUser($uid){
-        $chars = array();
-        $chars[0] = json_encode(array("uid"=>1,"charName"=>"Olaf", "charLvl"=>"1"));
-        $chars[1] = json_encode(array("uid"=>2,"charName"=>"Torben", "charLvl"=>"3"));
-
-        return $chars;
-    }
-?>
