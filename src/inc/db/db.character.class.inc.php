71c0d80176665e77a725cf1c8fc984d4afcf35f4
Luis Eckert <eckert.luis@gmx.de>
2022-01-04 21:31:04 +0100

    Create db.character.class.inc.php

---
+++ src/inc/db/db.character.class.inc.php
@@ -0,0 +1,23 @@
+<?php
+
+class Character {
+
+    public $name;
+    public $_id;
+    public $user_id;
+    public $class;
+    public $spells;
+    public $level;
+
+    function __construct() {
+        $spells = array();
+
+    }
+
+     // 1. Char aud datenbank geladen
+   // 2.Char wird erstellt
+   //     2.2 neuer datenbank eintrag und der wird dann neu eingelesen
+    
+    
+
+}
