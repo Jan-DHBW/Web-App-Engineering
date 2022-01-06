5b803ab960c796a70c797e65f4480abb6f10ed7c
Luis Eckert <eckert.luis@gmx.de>
2022-01-06 19:08:13 +0100

    Create db.user.sendAuthEmail.inc.php

---
+++ src/inc/db/db.user.sendAuthEmail.inc.php
@@ -0,0 +1,25 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+function db_user_sendAuthEmail($con, $name, $email, $pwdHash)
+{
+    $mail = new PHPMailer(true);
+
+    $mail->IsSMTP(); // telling the class to use SMTP
+    $mail->SMTPDebug = 0;
+    $mail->SMTPAuth = true; // enable SMTP authentication
+    $mail->SMTPSecure = "tls"; // sets the prefix to the servier
+    $mail->Host = "smtp.gmail.com"; // sets GMAIL as the SMTP server
+    $mail->Port = 587; // set the SMTP port for the GMAIL server
+    $mail->Username = "my.dnd.spellbook@gmail.com"; // GMAIL username
+    $mail->Password = "29!20BWHdmQS_p"; // GMAIL password
+
+    $mail->setFrom("my.dnd.spellbook@gmail.com", "dnd spellbook");
+    $mail->addAddress($email, $name);
+    $mail->Subject = 'DnD Spellbook email authentification';
+    $mail->Body = 'Please confirm the following link: <a href="http://'.$_SERVER["SERVER_NAME"].'/auth.php?auth='.$pwdHash.'">'.$_SERVER["SERVER_NAME"].'/auth.php?auth='.$pwdHash.'</a>';
+
+    return $mail->send();
+}
