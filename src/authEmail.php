d270b26b862f3c632cdbbc97154dc8b7d459de53
Luis Eckert <eckert.luis@gmx.de>
2022-01-09 19:00:29 +0100

    authEmail validate token

--- src/authEmail.php
+++ src/authEmail.php
@@ -3,12 +3,19 @@
 if(isset($_GET['token'])){
     //requirements
     require_once("inc/db/dbh.inc.php");
+    require_once("inc/db/db.user.isValidHashTokenAuthEmail.inc.php");
     require_once("inc/hash.inc.php");
+    
 
     //sanatize parameter
     $token = sanitizeHashToken($_GET['token']);
 
+    //TODO: check token valid
     //TODO: check toke expired
+    if(isTokenValid($DB, $token) === false){
+        header('location: errorPage.php?err=pageNotFound');
+        exit();
+    }
 
     $collection = $DB->users;
 
@@ -23,4 +30,8 @@ if(isset($_GET['token'])){
     );
 
     //redirect to other site
+}
+
+function isTokenValid($con, $token){
+    return db_user_isValidHashTokenAuthEmail($con, $token);
 }
