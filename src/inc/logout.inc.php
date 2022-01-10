11af9d459fc012ad4b18fc7128e2f11caead7879
Luis Eckert <eckert.luis@gmx.de>
2022-01-10 19:21:52 +0100

    clear cache on logout

--- src/inc/logout.inc.php
+++ src/inc/logout.inc.php
@@ -7,6 +7,9 @@ unset($_SESSION['username']);
 
 session_destroy();
 
+header('Cache-Control: no-cache, must-revalidate');
+header('Expires: Sat, 26 Jul 1997 05:00:00 GMT');
+
 header("location:/");
 
 ?>
