eb4cb06511a2560ce436b871b1dc28d9214d3893
Luis Eckert <eckert.luis@gmx.de>
2022-01-10 08:42:14 +0100

    Update db.char.updateCharacter.php

--- src/inc/db/db.char.updateCharacter.php
+++ src/inc/db/db.char.updateCharacter.php
@@ -1,11 +1,12 @@
 <?php
 
 
-function db_char_updateCharacter($con, $cid, $name, $level, $class){
+function db_char_updateCharacter($con, $cid, $uid, $name, $level, $class){
     $collection = $con->characters;
 
     $updateOneResult = $collection->updateOne(
         ['cid' => ['$eq' => $cid]],
+        ['uid' => ['$eq' => $uid]],
         ['$set' => [
             'name' => $name,
             'class' => $class,
