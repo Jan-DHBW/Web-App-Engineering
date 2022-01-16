993928d5f0cb2eb49c7c21a5b27bb0ddb8e76860
Jan A <>
2022-01-16 12:32:57 +0100

    charspells in getSpells

--- src/inc/db/db.spell.getSpell.inc.php
+++ src/inc/db/db.spell.getSpell.inc.php
@@ -10,24 +10,49 @@ function getSpell($spellid){
     $bspellid = new MongoDB\BSON\ObjectID($spellid);
     }
     $filter = ['_id' =>  $bspellid];
-    $result = $collection->find($filter);
-    $j = json_decode(MongoDB\BSON\toJSON(MongoDB\BSON\fromPHP($result)), true);
+    $result = $collection->findOne($filter);
+    if ($result != null){
+        $j = json_decode(MongoDB\BSON\toJSON(MongoDB\BSON\fromPHP($result)), true);
 
-    $tmpspell->_id = isset($j['_id']) ? $j['_id']['$oid'] : null;
-    $tmpspell->index = isset($j['index']) ? $j['index'] : null;
-    $tmpspell->name = isset($j['name']) ? $j['name'] : null;
-    $tmpspell->desc = isset($j['desc']) ? implode(' ', $j['desc']) : null;
-    $tmpspell->range = isset($j['range']) ? $j['range'] : null;
-    $tmpspell->components = isset($j['components']) ? implode(', ',$j['components']) : null;
-    $tmpspell->material = isset($j['material']) ? $j['material'] : null;
-    $tmpspell->ritual = isset($j['ritual']) ? $j['ritual'] : null;
-    $tmpspell->duration = isset($j['duration']) ? $j['duration'] : null;
-    $tmpspell->concentration = isset($j['concentration']) ? $j['concentration'] : null;
-    $tmpspell->level = isset($j['level']) ? $j['level'] : null;
-    $tmpspell->attack_type = isset($j['attack_type']) ? $j['attack_type'] : null;
-    $tmpspell->school = isset($j['school']) ? $j['school']['name'] : null;
-    $tmpspell->area_of_effect = isset($j['area_of_effect']) ? implode(', ', $j['area_of_effect']) : null;
-    return $tmpspell;
+        $tmpspell->_id = isset($j['_id']) ? $j['_id']['$oid'] : null;
+        $tmpspell->index = isset($j['index']) ? $j['index'] : null;
+        $tmpspell->name = isset($j['name']) ? $j['name'] : null;
+        $tmpspell->desc = isset($j['desc']) ? implode(' ', $j['desc']) : null;
+        $tmpspell->range = isset($j['range']) ? $j['range'] : null;
+        $tmpspell->components = isset($j['components']) ? implode(', ',$j['components']) : null;
+        $tmpspell->material = isset($j['material']) ? $j['material'] : null;
+        $tmpspell->ritual = isset($j['ritual']) ? $j['ritual'] : null;
+        $tmpspell->duration = isset($j['duration']) ? $j['duration'] : null;
+        $tmpspell->concentration = isset($j['concentration']) ? $j['concentration'] : null;
+        $tmpspell->level = isset($j['level']) ? $j['level'] : null;
+        $tmpspell->attack_type = isset($j['attack_type']) ? $j['attack_type'] : null;
+        $tmpspell->school = isset($j['school']) ? $j['school']['name'] : null;
+        $tmpspell->area_of_effect = isset($j['area_of_effect']) ? implode(', ', $j['area_of_effect']) : null;
+        return $tmpspell;
+    }
+    $col = "charspells";
+    $collection = $DB->$col;
+    $result = $collection->findOne($filter);
+    if ($result != null){
+        $j = json_decode(MongoDB\BSON\toJSON(MongoDB\BSON\fromPHP($result)), true);
+
+        $tmpspell->_id = isset($j['_id']) ? $j['_id']['$oid'] : null;
+        $tmpspell->index = isset($j['index']) ? $j['index'] : null;
+        $tmpspell->name = isset($j['name']) ? $j['name'] : null;
+        $tmpspell->desc = isset($j['desc']) ? implode(' ', $j['desc']) : null;
+        $tmpspell->range = isset($j['range']) ? $j['range'] : null;
+        $tmpspell->components = isset($j['components']) ? implode(', ',$j['components']) : null;
+        $tmpspell->material = isset($j['material']) ? $j['material'] : null;
+        $tmpspell->ritual = isset($j['ritual']) ? $j['ritual'] : null;
+        $tmpspell->duration = isset($j['duration']) ? $j['duration'] : null;
+        $tmpspell->concentration = isset($j['concentration']) ? $j['concentration'] : null;
+        $tmpspell->level = isset($j['level']) ? $j['level'] : null;
+        $tmpspell->attack_type = isset($j['attack_type']) ? $j['attack_type'] : null;
+        $tmpspell->school = isset($j['school']) ? $j['school']['name'] : null;
+        $tmpspell->area_of_effect = isset($j['area_of_effect']) ? implode(', ', $j['area_of_effect']) : null;
+        return $tmpspell;
+    }
+    return null;
 }
 
 
--- src/inc/test.php
+++ src/inc/test.php
@@ -30,19 +30,39 @@ function createDbConnection(){
     // }
 
     $cid = 'LVlp0RzGXqJd8rhMAqxfs9RXOpKB57yCqQjKM7vw0';
-    $spellid = '61db0f92246f56da35983e9b';
+    $spellid = '61db9f92247f56da35983e8b';
     //$col = 'characters'
     //$collection = $DB->$col;
     $bspellid = new MongoDB\BSON\ObjectID($spellid);
 
-    $updateOneResult = $collection->updateOne(
-        ['cid' => $cid, 'spells.spell_id'=> $bspellid],
-        ['$set' => [
-                'spells.$.prepared' => false
-            ]
-        ]
-    );
+    $tmpspell = new Spell();
+    $col = "spells";
+    $collection = $DB->$col;
+    if ($spellid instanceof MongoDB\BSON\ObjectID) {
+        $bspellid = $spellid;
+    } else {
+    $bspellid = new MongoDB\BSON\ObjectID($spellid);
+    }
+    $filter = ['_id' =>  $bspellid];
+    $result = $collection->findOne($filter);
+    if($resu)
+    $j = json_decode(MongoDB\BSON\toJSON(MongoDB\BSON\fromPHP($result)), true);
 
+    $tmpspell->_id = isset($j['_id']) ? $j['_id']['$oid'] : null;
+    $tmpspell->index = isset($j['index']) ? $j['index'] : null;
+    $tmpspell->name = isset($j['name']) ? $j['name'] : null;
+    $tmpspell->desc = isset($j['desc']) ? implode(' ', $j['desc']) : null;
+    $tmpspell->range = isset($j['range']) ? $j['range'] : null;
+    $tmpspell->components = isset($j['components']) ? implode(', ',$j['components']) : null;
+    $tmpspell->material = isset($j['material']) ? $j['material'] : null;
+    $tmpspell->ritual = isset($j['ritual']) ? $j['ritual'] : null;
+    $tmpspell->duration = isset($j['duration']) ? $j['duration'] : null;
+    $tmpspell->concentration = isset($j['concentration']) ? $j['concentration'] : null;
+    $tmpspell->level = isset($j['level']) ? $j['level'] : null;
+    $tmpspell->attack_type = isset($j['attack_type']) ? $j['attack_type'] : null;
+    $tmpspell->school = isset($j['school']) ? $j['school']['name'] : null;
+    $tmpspell->area_of_effect = isset($j['area_of_effect']) ? implode(', ', $j['area_of_effect']) : null;
+    
 
         printf("sdfa");
     
